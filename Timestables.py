300c25d3ef75b1c5e8dad90bda54e8c9d37fb4ae
Phoenix N <pn22178@my.westlake.school.nz>
2025-04-30 12:48:20 +1200

    Completed Timestables.py Activity with error prevention, infinite timestable capability, and reuse ability

---
+++ Timestables.py
@@ -0,0 +1,35 @@
+x = ""
+y = ""
+cycle = 0
+
+def timestables():
+    while True:
+        x = input("Enter a times tables number: ")
+        if x.isdigit():
+            x = int(x)
+            break
+
+
+    while True:
+        y = input(f"Enter a number of multiples of {x}: ")
+        if y.isdigit():
+            y = int(y)
+            break
+
+    n = 0
+    for n in range(n + 1, y + 1):
+        print(x, " x ", n, " = ", x * n)
+    
+    global cycle
+    cycle = cycle + 1
+
+while True:
+    if cycle == 0:
+        timestables()
+    else:
+        restart = input("Would you like another times table?(y/n): ")
+        yes_answer = ["y", "Y", "yes", "Yes"]
+        if restart in yes_answer:
+            timestables()
+        else:
+            break
