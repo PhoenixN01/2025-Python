311711771a8a1b90cedabb053f7b6700953dd760
Phoenix N <pn22178@my.westlake.school.nz>
2025-05-09 12:53:38 +1200

    Almost finished the creation of the Jar Jar Legacy

---
+++ Star-Wars.py
@@ -0,0 +1,12 @@
+JarJar = {
+    "Name": "Jar Jar Binks",
+    "Age": 86
+}
+
+JarJar["Class"] = "Traitor"
+JarJar["Location"] = ["Naboo", "Coruscant", "Gungan City"]
+JarJar["Weapons"] = ["Clumsy", "3rd Sith"]
+JarJar["Tasks"] = {"Obvious": "Be Clumsy", "Revealed": "Represent Naboo", "Actual": "Assist the creation of The Empire"}
+
+print("Jar Jar Binks' Actual task was to: ", JarJar["Tasks"]["Revealed", "Actual"])
+
