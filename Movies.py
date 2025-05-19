d38deedd7bcf02f6fb7deb0f317c61020c217f9e
Phoenix N <pn22178@my.westlake.school.nz>
2025-05-19 12:48:10 +1200

    Movies.py - Buy Tickets

--- Movies.py
+++ Movies.py
@@ -1,19 +1,19 @@
-from easygui import *
+import easygui
 
 movies = {
     "Inception": {
-        "title": "Inception",
-        "genre": "Sci-Fi",
-        "duration": 148,
-        "seats": 85,
-        "rating": 6,
-        "reviews": {
+        "Title": "Inception",
+        "Genre": "Sci-Fi",
+        "Duration": 148,
+        "Seats": 85,
+        "Rating": 6,
+        "Reviews": {
             1: {"name": "Adam", "rating": 6, "comment": "Amazing plot!"},
         },
-        "price": 12,
+        "Price": 12,
     },
     "Interstellar": {
-        "title": "Interstellar",
+        "Title": "Interstellar",
         "genre": "Sci-Fi",
         "duration": 169,
         "seats": 110,
@@ -24,7 +24,7 @@ movies = {
         "price": 13,
     },
     "Joker": {
-        "title": "Joker",
+        "Title": "Joker",
         "genre": "Drama",
         "duration": 122,
         "seats": 100,
@@ -37,7 +37,7 @@ movies = {
 }
  
 users = {
-    "admin": {"password": "unbirvwb"},
+    "admin": {"password": "unbirvwb", "balance": 999999999999.99},
     "Billy": {"password": "billy", "balance": 1250.50},
     "Bobby": {"password": "bobby", "balance": 820.00},
     "Bo": {"password": "bo", "balance": 312.75},
@@ -85,33 +85,48 @@ def login(users, current_user, logged_in):
         print("Login attempt Timeout. Please try again later")
     return users, current_user, logged_in
 
-def search_movies(target, movies=dict):
+def search_movies(movie_id, movies):
     found = False
     for i in movies:
-        if movies[i] == target:
-            print(target, " Found", sep="")
-            print(movies[target])
+        if movies[i] == movie_id:
+            print(movie_id, " Found", sep="")
+            print(movies[movie_id])
             found = True
     if found == False:
         print("Movie Not Found")
-    return
+    return movie_id, movies
 
-def buy_ticket(users=dict, movies=dict, current_user=str):
+def buy_ticket(users, movies, current_user):
     print("Available Movies:")
     for movie in movies.keys():
         print(movie)
     while True:
         selection = input("Which movie would you like to purchase tickets for? ")
         if selection in movies.keys():
-            seats = "seats"
-            price = "price"
-            seats_available = int(movies[selection][seats])
-            ticket_price = int(movies[selection][price])
+            seats_available = int(movies[selection]["Seats"])
+            ticket_price = int(movies[selection]["Price"])
+            user_balance = users[current_user]["Balance"]
             if seats_available > 0:
                 while True:
                     seats_wanted = input("How many seats would you like to purchase? ")
                     if seats_wanted.isdigit():
                         seats_wanted = int(seats_wanted)
+                        if seats_wanted <= seats_available:
+                            booking_price = seats_wanted * ticket_price
+                            if user_balance >= booking_price:
+                                user_balance -= booking_price
+                                seats_available -= seats_wanted
+                                print("Purchase Successful")
+                                print("Would you like to purchase some more tickets?")
+                                movies[selection]["Seats"] = seats_available
+                                users[current_user]["Balance"] = user_balance
+                            else:
+                                print(f"You have insufficient Funds, you need ${booking_price} to book this amount of seats. Current Balance: ${user_balance}")
+                                exit = input("Would you like to try again? (y/n)")
+                                if not exit == "y":
+                                    break
+                        else:
+                            print(f"There are only {seats_available} seats available")
                     else:
                         print("Purchase Amount must be an Integer")
             else:
@@ -122,24 +137,25 @@ def buy_ticket(users=dict, movies=dict, current_user=str):
             print("Movie Not Found")
     return users, movies
 
-def add_movie(movies=dict):
+def add_movie(movies):
     while True:
-        title = input("Movie Title: ")
-        genre = input("Movie Genre: ")
-        duration = input("Movie Duration: ")
-        seats = input("Seats allocated for Movie: ")
-        price = input("Ticket Price for Movie: ")
+
+        movie_title = input("Movie Title: ")
+        movie_genre = input("Movie Genre: ")
+        movie_duration = input("Movie Duration: ")
+        movie_seats = input("Seats allocated for Movie: ")
+        movie_price = input("Ticket Price for Movie: ")
         print()
         print("New Movie Details: ")
-        print("Title: ", title)
-        print("Genre: ", genre)
-        print("Duration: ", duration)
-        print("Number of Seats: ", seats)
-        print("Ticket Price: ", price)
+        print("Title: ", movie_title)
+        print("Genre: ", movie_genre)
+        print("Duration: ", movie_duration)
+        print("Number of Seats: ", movie_seats)
+        print("Ticket Price: ", movie_price)
         while True:
             confirmation = input("Cofirm? (y/n): ")
             if confirmation == "y":
-                movies[title] = {"title": title, "genre": genre, "duration": duration, "seats": seats, "ratings(Empty)": {}, "price": price}
+                movies[movie_title] = {"Title": movie_title, "Genre": movie_genre, "Duration": movie_duration, "Seats": movie_seats, "Rating": 0, 'Reviews': {}, "price": movie_price}
                 return movies
             elif confirmation == "n":
                 retry = input("Would you like to try again? (y/n): ")
@@ -152,26 +168,21 @@ def add_movie(movies=dict):
             else:
                 print("Invalid Response")
 
-def edit_movie(movies=dict):
+def edit_movie(movies):
     count = 0
     while True:
         target = input("Movie to Edit: ")
         if target in movies.keys():
             print(target, " Selected.")
             print("Movie Details: ")
-            title = "title"
-            print("[0] Title: ", movies[target][title])
-            genre = "genre"
-            print("[1] Genre: ", movies[target][genre])
-            duration = "duration"
-            print("[2] Duration: ", movies[target][duration])
-            seats = "seats"
-            print("[3] Seats: ", movies[target][seats])
-            reviews = "reviews"
-            print("[4] Reviews", movies[target][reviews])
-            price = "price"
-            print("[5] Price: ", movies[target][price])
-            options = [title, genre, duration, seats, reviews, price]
+            print("[0] Title: ", movies[target]["Title"])
+            print("[1] Genre: ", movies[target]["Genre"])
+            print("[2] Duration: ", movies[target]["Duration"])
+            print("[3] Seats: ", movies[target]["Seats"])
+            print("[4] Rating: ", movies[target]["Rating"])
+            print("[5] Reviews: ", movies[target]["Reviews"])
+            print("[6] Price: ", movies[target]["Price"])
+            options = ["Title", "Genre", "Duration", "Seats", "Reviews", "Price"]
             while True:
                 selection = input("Item to edit [0-5]. [EXIT] to cancel: ")
                 if selection.isdigit():
@@ -201,7 +212,7 @@ def edit_movie(movies=dict):
             else:
                 print("Invalid Response")
 
-def view_users(users=dict):
+def view_users(users):
     print("Here are the current list of users")
     user_display = list(users.keys())
     user_display.pop(0)
@@ -214,7 +225,7 @@ def view_users(users=dict):
     else:
         return users
 
-def edit_users(users=dict):
+def edit_users(users):
     while True:
         user = input("Which user would you like to edit: ")
         user_info = users[user]
@@ -269,7 +280,7 @@ def admin_account(users, movies):
 def user_account(users, movies, current_user):
     while True:
         print("Welcome to your user account, Below is a list of things that you can do in this account. To Select an option, just enter the id of that option to continue")
-        action_list = ["Find a Movie [0]", "Purchase a Movie Ticket [1]", "Write a review [2]"]
+        action_list = ["Find a Movie [0]", "Purchase a Movie Ticket [1]", "Write a review [2]", "View Account Details [3]", "Exit [4]"]
         print(*action_list, sep=", ")
         action = input("What would you like to do?")
         if action.isdigit():
@@ -277,12 +288,18 @@ def user_account(users, movies, current_user):
             if action == 0:
                 print("Here is a list of the current viewing Movies: ")
                 print(*movies.keys(), sep=", ")
-                target = input("Which movie details would you like to find? ")
-                search_movies(target, movies)
+                movie_id = input("Which movie details would you like to find (select movie by its id)? ")
+                movie_id, movies = search_movies(movie_id, movies)
+                print("Returning to the User Menu...")
             elif action == 1:
                 users, movies = buy_ticket(users, movies, current_user)
+                print("Returning to the User Menu...")
+            elif action == 4:
+                return users, movies, current_user
         else:
             print("Invalid Id")
+        break
+    return users, movies, current_user
 
 def main(users, movies, current_user, logged_in):
     users, current_user, logged_in = login(users, current_user, logged_in)
@@ -290,9 +307,9 @@ def main(users, movies, current_user, logged_in):
         if current_user == "admin":
             account = input("Would you like to access User or Admin account? ")
             if account == "Admin":
-                admin_account(users, movies, current_user)
+                users, movies = admin_account(users, movies)
             elif account == "User":
-                user_account(users, movies, current_user)
+                users, movies, current_user = user_account(users, movies, current_user)
         else:
             user_account(users, movies, current_user)
     return
